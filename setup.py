84b6ac1d1015050b98889ddde3e6d111e33cbdc1
Matis Alias-Bagarre <matis.alias-bagarre@inserm.fr>
2025-03-10 17:32:35 +0100

    remove setup.py install bdsg if not found

--- setup.py
+++ setup.py
@@ -1,27 +1,10 @@
 from setuptools import setup, find_packages
-import subprocess
-import os
-
-class CustomInstallCommand:
-    """Custom installation to include libbdsg installation."""
-    @staticmethod
-    def install_libbdsg():
-        try:
-            # Clone the repository with --recursive
-            subprocess.check_call(["git", "clone", "--recursive", "https://github.com/vgteam/libbdsg.git"])
-            os.chdir("libbdsg")
-            # Install using pip3
-            subprocess.check_call(["pip3", "install", "."])
-            os.chdir("..")  # Return to the original directory
-        except subprocess.CalledProcessError as e:
-            print(f"Failed to install libbdsg: {e}")
-            raise
 
 # Check if libbdsg is already installed
 try:
     import bdsg
 except ImportError:
-    CustomInstallCommand.install_libbdsg()
+   raise ImportError("ERROR bdsg not found")
 
 setup(
     name="STOAT",
