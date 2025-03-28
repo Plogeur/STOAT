34f8e5fd7aeea3bc522014428e7e9917323f2750
malias <matis84700@gmail.com>
2025-03-28 09:09:57 +0100

    tt

--- stoat/__main__.py
+++ stoat/__main__.py
@@ -12,7 +12,7 @@ import sys
 def main() : 
 
     # Argument Parsing
-    parser = argparse.ArgumentParser(description="Run the Stoat GWAS analysis pipeline")
+    parser = argparse.ArgumentParser(description="Run the Stoat GWAS analysis tool")
     parser.add_argument("-p", "--pg",type=utils.check_file, help='The input pangenome .pg file', required=False)
     parser.add_argument("-d", "--dist",type=utils.check_file, help='The input distance index .dist file', required=False)
     parser.add_argument("-n", "--name_chr",type=utils.check_file, help='The input chromosome prefix reference file', required=False)
@@ -160,7 +160,7 @@ Usage example:
  
 Usage test:
     stoat -p tests/simulation/binary/pg.full.pg -d tests/simulation/binary/pg.dist -v tests/simulation/binary/merged_output.vcf \
-    -b tests/simulation/binary/phenotype.tsv -o output
+    -b tests/simulation/binary/phenotype.tsv -o output --gaf
 
     stoat -p tests/simulation/quantitative/pg.full.pg -d tests/simulation/quantitative/pg.dist -v tests/simulation/quantitative/merged_output.vcf \
     -q tests/simulation/quantitative/phenotype.tsv -o output
