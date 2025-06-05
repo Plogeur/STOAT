1376a8b9f476cd295a13c9505f6c21d27786900a
Matis Alias-Bagarre <matis.alias-bagarre@inserm.fr>
2025-06-05 10:31:01 +0200

    tt

--- tests/unittest/list_snarl_paths_unit.cpp
+++ tests/unittest/list_snarl_paths_unit.cpp
@@ -283,11 +283,11 @@ TEST_CASE("Test simulated case", "[Path]") {
         REQUIRE(std::get<3>(snarls_chr["ref"][0]) == 10);
         REQUIRE(std::get<4>(snarls_chr["ref"][0]) == std::vector<std::string>{"3/8","6/16","1"});
 
-        REQUIRE(std::get<0>(snarls_chr["ref"][0]) == "3_6");
-        REQUIRE(std::get<1>(snarls_chr["ref"][0]) == std::vector<std::string>{">3>5>6",">3>4>6"});
-        REQUIRE(std::get<2>(snarls_chr["ref"][0]) == 8);
-        REQUIRE(std::get<3>(snarls_chr["ref"][0]) == 10);
-        REQUIRE(std::get<4>(snarls_chr["ref"][0]) == std::vector<std::string>{"2","1"});
+        REQUIRE(std::get<0>(snarls_chr["ref"][1]) == "3_6");
+        REQUIRE(std::get<1>(snarls_chr["ref"][1]) == std::vector<std::string>{">3>5>6",">3>4>6"});
+        REQUIRE(std::get<2>(snarls_chr["ref"][1]) == 8);
+        REQUIRE(std::get<3>(snarls_chr["ref"][1]) == 10);
+        REQUIRE(std::get<4>(snarls_chr["ref"][1]) == std::vector<std::string>{"2","1"});
     }
 
     SECTION("repetition") {
